d78e2099f1d65f6577f1a5ec451b981f0e82f054
Eric Clinton Appiah-Kubi <https://appiahmakuta70-outlook@dev.azure.com>
2020-02-12 13:22:14 +0000

    Changed staff id at logon to Username

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -244,7 +244,7 @@ $user = getUserSession();
                                     <div class="col-sm-12">
                                         <input type="text" id="staff_id" name="staff_id"
                                                class="<?php //echo !empty($payload['post']->staff_id_err)? 'border-danger-4': '' ?>"
-                                               placeholder="STAFF ID" aria-describedby="helpId"
+                                               placeholder="USERNAME" aria-describedby="helpId"
                                                value="<?php echo !empty($payload['post']->staff_id) ? $payload['post']->staff_id : '' ?>"
                                                required/>
                                         <small class="with-errors help-block d-block">
