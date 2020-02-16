4a1919fdc07774548eba20a45517bb43b823fde9
Eric Clinton Appiah-Kubi <https://appiahmakuta70-outlook@dev.azure.com>
2020-02-16 23:12:30 +0000

    Fixes after migration to CkEditor

--- app/controllers/FileService.php
+++ app/controllers/FileService.php
@@ -7,12 +7,12 @@ class FileService extends Controller
     public function read(): void
     {
         $path = isset($_POST['path']) ? $_POST['path'] : '';
-        echo readFileMetaData(FILE_UPLOAD_PATH . "/" . uniqueId() . "/" . $path);
+        echo readFileMetaData(FILE_UPLOAD_PATH . "/"  . $path);
     }
 
     public function upload(): void
     {
-        $current_user_file_upload_path = FILE_UPLOAD_PATH . '/' . uniqueId();
+        $current_user_file_upload_path = FILE_UPLOAD_PATH;
         $path = $_POST['path'] ? $current_user_file_upload_path . '/' .  $_POST['path'] : $current_user_file_upload_path;
         $file = uploadFile($path);
         if ($file->completed) {
@@ -24,14 +24,14 @@ class FileService extends Controller
     public function files()
     {
        // $file_name = FILE_UPLOAD_PATH . "/" . uniqueId() . "/" . strtolower($_GET['type']) . $_GET['currentFolder']  . $_GET['fileName'];
-        $file_name = FILE_UPLOAD_PATH . "/" . uniqueId() . $_GET['path'];
+        $file_name = FILE_UPLOAD_PATH . "/" . $_GET['path'];
         sendFile($file_name);
     }
 
     public function createDirectory()
     {
         $path = $_POST['path'] ? '/' . $_POST['path'] . $_POST['name'] : '/' . $_POST['name'];
-        $upload_path = FILE_UPLOAD_PATH . "/"  . uniqueId() . "/" . $path;
+        $upload_path = FILE_UPLOAD_PATH . "/" . $path;
         if (mkdir($upload_path, 0777, true)) {
             echo json_encode(['name' => $_POST['name'], 'type' => 'd', 'path' => $_POST['path']]);
         } else {
