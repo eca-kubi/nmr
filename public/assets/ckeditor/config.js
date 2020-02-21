6fed61ad6ef15cc578a7dfe24bed9095b635d8c0
Eric Clinton Appiah-Kubi <https://appiahmakuta70-outlook@dev.azure.com>
2020-02-21 09:40:03 +0000

    Fixes after migration to CkEditor

--- public/assets/ckeditor/config.js
+++ public/assets/ckeditor/config.js
@@ -19,4 +19,5 @@ CKEDITOR.editorConfig = function( config ) {
     config.autoGrow_bottomSpace = 50;
     config.extraPlugins = 'image2, toc, tabletoolstoolbar, tableresize, tableresizerowandcolumn';
     config.removePlugins = 'save, forms, preview, language, styles, iframe, specialchar, flash, about, bidi, newpage, stylescombo, div, preview, pastebase64';
+    config.fontSize_sizes = '8/8pt;9/9pt;10/10pt;11/11pt;12/12pt;14/14pt;16/16pt;18/18pt;20/20pt;22/22pt;24/24pt;26/26pt;28/28pt;36/36pt;48/48pt;72/72pt';
 };
