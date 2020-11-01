6678d7ae72888b6c183436dbf3d60a21cf647160
Eric Clinton Appiah-Kubi <https://appiahmakuta70-outlook@dev.azure.com>
2020-11-01 22:34:53 +0000

    Added functions to session_helper.php to set and get current controller and action.

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -156,5 +156,19 @@ function excludeReviewer()
     return Database::getInstance();
 }
 
-?>
+function setCurrentController (string $current_controller) {
+    $_SESSION['current_controller'] = $current_controller;
+}
+
+function setCurrentAction(string $current_action) {
+    $_SESSION['current_action'] = $current_action;
+}
+
+function getCurrentController() {
+    return $_SESSION['current_controller'];
+}
+
+function getCurrentAction () {
+    return $_SESSION['current_action'];
+}
 
